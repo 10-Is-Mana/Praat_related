2492b4d1310ba7cc36971ca02ce82d81e2f4b13e
10-Is-Mana <57948677+10-Is-Mana@users.noreply.github.com>
2020-09-12 00:24:10 +0900

    Add files via upload

---
+++ extract_tierinfo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# created by Mana ASHIDA, 11-09-2020, ver.1.0
+
+import glob
+import csv
+import textgrids
+
+print("What is the path to the folder containing target TextGrids?")
+path_in = input()
+paths = glob.glob(path_in+"/*.TextGrid")
+
+f = open(path_in+"/TierInfos.csv", "w")
+writer = csv.writer(f)
+
+for path in sorted(paths):
+    f_in = textgrids.TextGrid(path)
+    info = []
+    info.append(path.split("/")[-1])
+    for elem in f_in:
+        interval = []
+        for seg in f_in[elem]:
+            interval.append(seg.text)
+        info.append(" ".join(interval))
+    writer.writerow(info)
+
+f.close()
+print("TierInfos.csv is generated in "+path_in)
